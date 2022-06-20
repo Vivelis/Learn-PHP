cc120dd336b9a690209ad143c6c991d9c13635aa
Macgameur <maceo.jalbert@gmail.com>
2022-06-20 17:37:56 +0200

    :feat: update index.php and add footer and function file

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <?php
+            // require return a fatal error if file does not exist.
+            // _one include avoid to include many time same thing
+            require_one 'functions.php';
+        ?>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="style.css" rel="stylesheet" type="text/css" />    </head>
@@ -8,6 +13,7 @@
         <header>
         </header>
         <h1>hello you</h1>
+        <a href="https://www.youtube.com/watch?v=cGwSm8xDSwI">link to the course<br></a>
         <?php
             $name = "me";
             $my_array = array("element1", "element2", 3, 4.1);
@@ -26,6 +32,85 @@
             echo $my_array[3];
             echo $my_array[4];
             
+            echo "<h2>Associative array</h2>";
+            $assos_array = array("car" => 1, "ship" => 4.5);
+            echo $assos_array["car"];
+            foreach ($assos_array as $element => $value) {
+                echo "my ", $element, " has ", $value, "score<br>";
+            }
+
+            echo "<h2>Multidimensional arrays</h2>";
+            $multi_array = array(
+                "Exepensive" => array("Peugeot", "Mercedes"),
+                "Inexpensive" => array("Volvo", "Renaud")
+            );
+            foreach ($multi_array as $elem1 => $value1) {
+                echo "colomn is : {$elem1}<br>";
+                foreach ($value1 as $elem3 => $value2) {
+                    echo $elem3, ' is ', $value2, "<br>"; 
+                }
+            }
+
+            echo "<h2>Assignment operators</h2>";
+            $str = "sentence 1";
+
+            echo "normal operators : +=, -=, *=, *=, /=.<br>";
+            echo "but there is also .= that is concatenate and asign.<br>";
+            $str .= "sentence 2"; 
+            echo $str;
+
+            echo "<h2>Comparison operators</h2>";
+            $nbr1 = 11;
+            $nbr2 = 10;
+
+            echo "normal operators : <, >, <=, >=, ==, !=.<br>";
+            echo "but there is also <> that is same as !=.<br>";
+            echo "and there is also === that check if egal and check if same type too.<br>";
+
+            if ($nbr1 === $nbr2) {
+                echo "same value and same type<br>";
+            } else {
+                echo "not same value or same type<br>";
+            }
+
+            echo "there is also the Spaceship operator : <=>.<br>";
+            $result = $nbr1 <=> $nbr2;
+            if ($result == -1)
+                echo "nbr1 is less than nbr2";
+            if ($result == 0)
+                echo "the two values are egal";
+            if ($result == 1)
+                echo "nbr2 is less than nbr1";
+
+            echo "<h2>Logical operators</h2>";
+            echo "And == &&, Or == ||, Xor, !...<br>";
+            if (TRUE And TRUE && TRUE)
+                echo "is TRUE<br>";
+
+            echo "<h2>Functions</h2>";
+            echo "to create function, use the keyword function.<br>";
+            function exempleFunction($num1 = 1) {
+                for ($cnt = 0; $cnt < $num1; $cnt++)
+                    echo "exemple<br>";
+                return $num1;
+            }
+            exempleFunction(2);
+            echo "you can ask for reference with &.<br>";
+            function referenceExemple(&$x) {
+                $x += 10;
+            }
+            $y = 1;
+            echo "before y = {$y}<br>";
+            referenceExemple($y);
+            echo "after y = {$y}<br>";
+
+            echo "<h2>Macros</h2>";
+            echo "use define(arg1,arg2);<br>";
+            define("MY_MACRO", "value");
+            echo MY_MACRO;
+        ?>
+        <?php
+            include 'footer.php';
         ?>
     </body>
 </html>
